a1bd568e052bf96ad11d771ef11ad51035ce94ef
Brian  Hess <brianhess@durhambrands.com>
2023-12-14 14:53:11 -0700

    Adding to the login page and to the dashboard page as well as setting up the Tailwind Css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '@/app/ui/global.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
