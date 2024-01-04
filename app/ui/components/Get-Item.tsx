94b5b0b159b0f529311a4655d7a3f18f35e12d85
Brian  Hess <brianhess@durhambrands.com>
2024-01-04 08:21:02 -0700

    Fixing Errors

--- app/ui/components/Get-Item.tsx
+++ app/ui/components/Get-Item.tsx
@@ -1,8 +1,15 @@
-import prisma from '../../lib/prisma';
+'use client'
 
-export const getServerSideProps = async () => {
-    const users = await prisma.items.findMany()
-    console.log(users)
-  
-    return { props: { users } }
-  }
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json());
+
+
+export default function GetItem() {
+  //const { data, error, isLoading } = useSWR('/api/fetch-item', fetcher)
+  const { data, isLoading, error } = useSWR('/api/fetch-item', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  return <div>hello here is the data = {data.id}</div>
+}
--- app/ui/components/GetItemName.tsx
+++
@@ -1,9 +0,0 @@
-import useSWR from 'swr'
-
-export default function GetItem() {
-  const { data, error, isLoading } = useSWR('/api/fetch-item', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-  return <div>hello {data.itemname}!</div>
-}
