1a9fd351d6b743ff90773c9f39e8e67b5c249f48
Brian Hess <109031060+BrianHess213@users.noreply.github.com>
2024-01-04 18:29:37 -0700

    Finished adding Oauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,32 @@
-'use client'
 
-import React from "react";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0"
+import { redirect } from "next/navigation";
 
-function index() {
-  const { user, error, isLoading } = useUser();
+export default async function Home() {
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+  const session = await getSession();
 
-  if (user) {
-    console.log(user);
-    return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-        <br></br>
-        Your nickname is {user.nickname}.
-      </div>
-    );
+  if (session?.user) {
+    redirect("/dashboard");
   }
-  return <a href="/api/auth/login">Login</a>;
-}
 
-export default index;
+  return (
+  
+
+<section className="h-screen flex items-center justify-center bg-white dark:bg-gray-900">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Durham Brands</h1>
+        <h2 className="text-[30px] mb-8 text-gray-100 dark:text-gray-100">License Plate</h2>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="/api/auth/login" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Sign In With Company Email
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+</section>
+
+  )
+}
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 // Without a defined matcher, this one line applies next-auth
 // to the entire project
-export { default } from "next-auth/middleware"
+//export { default } from "next-auth/middleware"
 
 // Applies netx-auth only to matching routes - can be regex
 // Ref: https://nextjs.org/docs/app/building-your-applications/routing/middleware#matcher
 //export const config = { matcher: ["/extra", "/dashboard"] }
+
+import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+
+export default withMiddlewareAuthRequired();
+
+export const config = { matcher: ["/dashboard"] }
+
