6f709e36289275a241306f1b76af21dbd3bf5019
Brian Hess <109031060+BrianHess213@users.noreply.github.com>
2024-01-04 18:41:54 -0700

    Changing URL to work in vercel

--- app/ui/default/NewFooter.tsx
+++ app/ui/default/NewFooter.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 
         <nav className="print:hidden fixed bottom-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
 
-        <span className="p-3 block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="http://localhost:3000" className="hover:underline">Durham Brands</a>. All Rights Reserved.</span>
+        <span className="p-3 block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://nextjs-license-plate-osut1jjrg-brianhess213.vercel.app/" className="hover:underline">Durham Brands</a>. All Rights Reserved.</span>
       </nav>
 
 
