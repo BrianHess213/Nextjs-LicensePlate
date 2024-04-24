bf69149c06fd18cb2f803eee95b5ff9c179bb149
Brian  Hess <brianhess@durhambrands.com>
2024-04-24 08:09:46 -0600

    Adding XATA DB

--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -71,6 +71,7 @@ export default defineType({
           name: 'alt',
           type: 'string',
           title: 'Alternative Text',
+          description: 'Important for SEO and accessibility.',
         }
       ]
     }),
